3235eae929879fa2bc416f9527c1f250aebc787e
Chandler Griscom <cjgriscom@gmail.com>
2025-07-19 15:07:38 -0400

    Initial commit

---
+++ morajai-analysis/src/main/java/io/chandler/morajai/CopyCompleteResults.java
@@ -0,0 +1,10 @@
+package io.chandler.morajai;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class CopyCompleteResults {
+	public static void main(String[] args) throws Exception {
+		Path src = Paths.get("results");
+	}
+}
