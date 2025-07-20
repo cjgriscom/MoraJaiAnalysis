32ce6333a72193daba64c794111e9547569abbe1
Chandler Griscom <cjgriscom@gmail.com>
2025-07-20 09:56:12 -0400

    Filter results programs

--- morajai-analysis/src/main/java/io/chandler/morajai/CopyCompleteResults.java
+++ morajai-analysis/src/main/java/io/chandler/morajai/CopyCompleteResults.java
@@ -1,10 +1,111 @@
 package io.chandler.morajai;
 
+import java.io.FileWriter;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class CopyCompleteResults {
 	public static void main(String[] args) throws Exception {
 		Path src = Paths.get("results");
+		System.out.println(src.toAbsolutePath());
+
+		// Loop thru the txt files in the src directory
+		List<Path> txtFiles = new ArrayList<>();
+		Files.walk(src)
+			.filter(Files::isRegularFile)
+			.filter(path -> path.getFileName().toString().endsWith(".txt"))
+			.forEach(txtFiles::add);
+
+		TreeMap<Integer, JSONObject> results = new TreeMap<>();
+		int completeCount = 0;
+
+		for (Path txtFile : txtFiles) {
+			JSONObject entry = new JSONObject();
+			boolean iscomplete = false;
+			JSONObject resultMap = new JSONObject();
+			ArrayList<Integer> sizeMap = new ArrayList<>();
+			int maxDepth = 0;
+
+			Scanner scanner = new Scanner(txtFile.toFile());
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				String header;
+				if (line.startsWith(header = "Starting analysis for ")) {
+					line = line.substring(header.length());
+					String[] parts = line.split(" ");
+					String idx = parts[0];
+					String name = parts[1];
+					String executor = parts[3];
+					String pruned = parts[6];
+					entry.put("idx", Integer.parseInt(idx));
+					entry.put("name", name);
+					entry.put("executor", executor);
+					entry.put("pruned", Boolean.parseBoolean(pruned));
+				} else if (line.startsWith("Complete")) {
+					iscomplete = true;
+				} else if (line.startsWith(header = "Depth ")) {
+					line = line.substring(header.length());
+					String[] parts = line.split(" ");
+					String depth = parts[0];
+					String states = parts[2];
+					if (Integer.parseInt(depth) != sizeMap.size()) throw new RuntimeException("Depth mismatch");
+					sizeMap.add(Integer.parseInt(states));
+					maxDepth = Math.max(maxDepth, Integer.parseInt(depth));
+				} else if (line.startsWith("[")) {
+					JSONArray result = new JSONArray(line);
+					JSONArray resultArray;
+					String key = (sizeMap.size()) + "";
+					if (resultMap.has(key)) {
+						resultArray = resultMap.getJSONArray(key);
+					} else {
+						resultArray = new JSONArray();
+					}
+					resultArray.put(result);
+					resultMap.put(key, resultArray);
+				}
+			}
+
+			entry.put("maxDepth", maxDepth);
+			entry.put("sizeMap", sizeMap);
+			entry.put("resultMap", resultMap);
+
+			if (iscomplete) {
+				results.put(entry.getInt("idx"), entry);
+				completeCount++;
+			} else {
+				System.out.println("Incomplete: " + txtFile.getFileName());
+			}
+			scanner.close();
+		}
+
+		Path dest = src.resolve("results.json");
+		FileWriter fileWriter = new FileWriter(dest.toFile());
+		String delim = "{\n";
+
+		Iterator<Map.Entry<Integer, JSONObject>> iterator = results.entrySet().iterator();
+		while (iterator.hasNext()) {
+			fileWriter.write(delim);
+			Map.Entry<Integer, JSONObject> entry = iterator.next();
+			fileWriter.write("\"" + entry.getKey() + "\": ");
+			fileWriter.write(entry.getValue().toString());
+			
+			iterator.remove();
+			delim = "\n,\n";
+		}
+		fileWriter.write("\n}");
+		fileWriter.close();
+
+		System.out.println("Complete count: " + completeCount);
+		System.out.println("Wrote JSON to: " + dest.toAbsolutePath());
 	}
 }
---
+++ morajai-analysis/src/main/java/io/chandler/morajai/ResultStats.java
@@ -0,0 +1,89 @@
+package io.chandler.morajai;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.function.BiFunction;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class ResultStats {
+	public static void main(String[] args) throws Exception {
+		Path resultsFile = Paths.get("results", "results.json");
+
+		int numCPU = 0;
+		int numGPU = 0;
+		int noResults = 0;
+		int maxDepth = 0;
+		ArrayList<String> maxDepthNames = new ArrayList<>();
+
+		BiFunction<JSONArray, HashSet<String>, Boolean> filter = (data, colors) -> {
+			// return colors.contains("PI"); // 388_C_GY_C_BK_C_BU_C_BU - Experimental 1
+			return !colors.contains("YE") || !colors.contains("BK") || !colors.contains("PU");
+		};
+
+		// Read into JSONObject
+		JSONObject results = new JSONObject(Files.readString(resultsFile));
+
+		Iterator<String> keys = results.keys();
+		while (keys.hasNext()) {
+			String key = keys.next();
+			JSONObject result = results.getJSONObject(key);
+			//System.out.println(key + ": " + result.toString());
+
+			if (result.getString("executor").equals("CPU")) {
+				numCPU++;
+			} else if (result.getString("executor").equals("GPU")) {
+				numGPU++;
+			}
+
+			boolean containsMaxResults = result.getJSONObject("resultMap").has(""+result.getInt("maxDepth"));
+			
+			if (!containsMaxResults) {
+				System.out.println("No results at max depth for " + result.getInt("idx") + result.getString("name"));
+				noResults++;
+				continue;
+			}
+
+			boolean acceptedResult = false;
+
+			JSONArray resultsAtMax = 
+				result.getJSONObject("resultMap").getJSONArray(""+result.getInt("maxDepth"));
+			for (int i = 0; i < resultsAtMax.length(); i++) {
+				JSONArray resultAtMax = resultsAtMax.getJSONArray(i);
+				HashSet<String> colors = new HashSet<>();
+				for (int j = 0; j < resultAtMax.length(); j++) {
+					String color = resultAtMax.getString(j);
+					colors.add(color);
+				}
+
+				if (filter.apply(resultAtMax, colors)) {
+					acceptedResult = true;
+					break;
+				}
+			}
+
+			if (!acceptedResult) continue;
+
+			if (result.getInt("maxDepth") > maxDepth) {
+				maxDepth = result.getInt("maxDepth");
+				maxDepthNames.clear();
+				maxDepthNames.add(result.getInt("idx") + result.getString("name"));
+			} else if (result.getInt("maxDepth") == maxDepth) {
+				maxDepthNames.add(result.getInt("idx") + result.getString("name"));
+			}
+
+			//break;
+		}
+
+		System.out.println("Num CPU: " + numCPU);
+		System.out.println("Num GPU: " + numGPU);
+		System.out.println("No results: " + noResults);
+		System.out.println("Max depth: " + maxDepth);
+		System.out.println("Max depth names: " + maxDepthNames);
+	}
+}
